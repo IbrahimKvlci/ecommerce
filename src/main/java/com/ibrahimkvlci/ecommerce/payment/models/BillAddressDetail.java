89dba359494ec3404c2b7a65eef797e530c5a67d
Halil İbrahim Kavalcı <110342285+IbrahimKvlci@users.noreply.github.com>
2025-09-21 14:17:22 +0300

    Add BillAddressDetail entity for managing billing address details in the payment module.

---
+++ src/main/java/com/ibrahimkvlci/ecommerce/payment/models/BillAddressDetail.java
@@ -0,0 +1,28 @@
+package com.ibrahimkvlci.ecommerce.payment.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "bill_address_details")
+public class BillAddressDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "address_detail_id", nullable = false)
+    private Long addressDetailId;
+
+
+}
