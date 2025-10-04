b6c2742db76b930d9c2c1f3343dec5c6606aa760
Halil İbrahim Kavalcı <110342285+IbrahimKvlci@users.noreply.github.com>
2025-10-04 22:12:58 +0300

    Remove Spring @Service annotation from CustomerService interface

--- src/main/java/com/ibrahimkvlci/ecommerce/auth/services/CustomerService.java
+++ src/main/java/com/ibrahimkvlci/ecommerce/auth/services/CustomerService.java
@@ -1,14 +1,11 @@
 package com.ibrahimkvlci.ecommerce.auth.services;
 
-import org.springframework.stereotype.Service;
-
 import com.ibrahimkvlci.ecommerce.auth.dto.RegisterCustomerRequest;
 import com.ibrahimkvlci.ecommerce.auth.dto.RegisterCustomerResponse;
 import com.ibrahimkvlci.ecommerce.auth.dto.CustomerDTO;
 
 import java.util.Optional;
 
-@Service
 public interface CustomerService {
     
     RegisterCustomerResponse registerAsCustomer(RegisterCustomerRequest request);
